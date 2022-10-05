5bf011ebda556f246b69459d442aa71d6118080b
Austinejnr <samuelamoto024@gmail.com>
2022-10-05 13:22:44 -0800

    almost comleted

---
+++ starter/main.js
@@ -0,0 +1,19 @@
+
+
+ScrollReveal({ reset: true,
+    duration: 2000,
+  distance: "70px"
+});
+    ScrollReveal().reveal('.heading', { delay: 500,
+    origin: "left"
+  });
+
+  ScrollReveal().reveal('.logo', { delay: 500,
+  origin: "right" });
+
+  ScrollReveal().reveal('.first-leftdiv p', { delay: 500,
+  origin: "bottom" });
+
+  ScrollReveal().reveal('.footericons-container i', { delay: 500,
+  origin: "top",
+interval: 150 });
